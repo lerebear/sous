fdb6fdbd6476f3f0cf119628cf64a61f7bcba70d
Lérè Williams <lere.williams@gmail.com>
2025-09-27 20:18:29 -0700

    Improve type for test cases

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -2,18 +2,20 @@ import unittest
 
 from sous.ingredient import Ingredient
 
+type IngredientArguments = tuple[str, str | None, str | None, str | None]
+
 
 class TestIngredient(unittest.TestCase):
     def test_parse_block_definition(self) -> None:
-        test_cases: list[tuple[str, dict[str, str | None]]] = [
+        test_cases: list[tuple[str, IngredientArguments]] = [
             (
                 "{3 cloves}[garlic], minced",
-                [
+                (
                     "garlic",
                     "3 cloves",
                     None,
                     "minced",
-                ],
+                ),
             )
         ]
 
